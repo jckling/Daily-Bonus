fcbd8631ce67141743062b8d9a74b782c4e50aea
jckling <jckling@163.com>
2021-08-26 15:03:25 +0800

    🔧 修复解析问题

--- Yamibo/yamibo_checkin.py
+++ Yamibo/yamibo_checkin.py
@@ -6,35 +6,43 @@
 import os
 
 import requests
+from bs4 import BeautifulSoup
 from lxml import html
 
 # cookies
 COOKIES = os.environ.get("YAMIBO_COOKIES")
-
 SESSION = requests.Session()
 
 HEADERS = {
-    "Host": "bbs.yamibo.com",
-    "Connection": "keep-alive",
-    "sec-ch-ua": '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-    "sec-ch-ua-mobile": "?0",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-User": "?1",
-    "Sec-Fetch-Dest": "document",
-    "Referer": "https://bbs.yamibo.com/forum.php",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,zh-TW;q=0.6,da;q=0.5",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+    "Referer": "https://bbs.yamibo.com/forum.php",
+    "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "sec-ch-ua-mobile": "?0",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "Connection": "keep-alive",
+    "Host": "bbs.yamibo.com",
     "Cookie": COOKIES
 }
 
 
+def fhash():
+    url = "https://bbs.yamibo.com/forum.php"
+    r = SESSION.get(url, headers=HEADERS)
+    tree = html.fromstring(r.text)
+    hash = tree.xpath('//input[@name="formhash"]')[0].attrib['value']
+    return hash
+
+
 # 签到
 def check_in():
-    url = "https://bbs.yamibo.com/plugin.php?id=study_daily_attendance:daily_attendance&fhash=41c1cbf1"
+    url = "https://bbs.yamibo.com/plugin.php?id=study_daily_attendance:daily_attendance&fhash=" + fhash()
     r = SESSION.get(url, headers=HEADERS)
 
     if "签到成功" in r.text:
@@ -54,7 +62,8 @@ def query_credit():
     url = "https://bbs.yamibo.com/home.php?mod=spacecp&ac=credit&op=base"
     r = SESSION.get(url, headers=HEADERS)
 
-    tree = html.fromstring(r.text)
+    soup = BeautifulSoup(r.text, "lxml")
+    tree = html.fromstring(str(soup))
     credit = tree.xpath('//ul[@class="creditl mtm bbda cl"]/li/text()')
     print("对象:\t %s\t\n"
           "积分:\t %s\t\n"
