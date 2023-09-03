29ca7ecc55eb70c2720363616be486851bca9e72
jckling <jckling@163.com>
2023-09-03 14:09:04 +0800

    修复百合会签到

--- Yamibo/yamibo_checkin.py
+++ Yamibo/yamibo_checkin.py
@@ -16,6 +16,14 @@ COOKIES = {
     "EeqY_2132_auth": os.environ.get("YAMIBO_EEQY_2132_AUTH"),
 }
 
+# headers
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,zh-TW;q=0.6",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+}
+
 # session
 SESSION = requests.Session()
 
@@ -92,6 +100,7 @@ def query_credit():
 
 def main():
     SESSION.cookies.update(COOKIES)
+    SESSION.headers.update(HEADERS)
     if check_in():
         query_credit()
     global msg
